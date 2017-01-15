94b5bb5751aae54c1abeeebab679d3f5fb43dadf
entreprenerder <abilitycreation@gmail.com>
2017-01-15 12:01:56 -0500

    global style rules set up

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import radium from 'radium';
+import radium, {Style} from 'radium';
 import styles from '../../src/styles/App.js';
 
 @radium
@@ -7,7 +7,10 @@ class App extends React.Component {
 
   render() {
     return (
-      <div style={styles.app}>Hello React</div>
+      <div style={styles.app}>
+        <div>Hello World</div>
+        {siteRules.map((rules, index) => <Style key={`style-rule-${index}`} rules={rules} />)}
+      </div>
     );
   }
 
