199980fab1f49e46a0472bbaeb994a0a80dbb8da
entreprenerder <abilitycreation@gmail.com>
2017-06-04 08:46:34 -0400

    bootstrap integration

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
+// React
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {HashRouter as Router, Route} from 'react-router-dom';
 
+// JS Libraries
+import jquery from 'jquery';
+import bootstrap from 'bootstrap';
+
+// Stylesheets
+import './../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './../node_modules/font-awesome/css/font-awesome.css';
+import './../node_modules/animate.css/animate.min.css';
+
+// Components
 import App from './components/App';
 
 ReactDOM.render(
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,49 @@
+var path = require('path');
+var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
   entry: './src/index.js',
   output: {
     path: __dirname + '/public',
     filename: 'bundle.js'
   },
+  plugins: [
+    new ExtractTextPlugin("styles.css"),
+    // new webpack.HotModuleReplacementPlugin(),
+    new webpack.ProvidePlugin({
+      '$': "jquery",
+      'jQuery': "jquery",
+      'window.jQuery': "jquery",
+      'window.$': 'jquery'
+    }),
+    new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.js', Infinity),
+    new webpack.DefinePlugin({
+        "require.specified": "require.resolve"
+    })
+  ],
   module: {
     loaders: [
       {
-        test: /\.json$/,
-        exclude: /(node_modules)/,
-        loader: 'json-loader'
-      },
-      {
         test: /\.js$/,
         loader: 'babel-loader',
-        exclude: /(node_modules)/,
         presets: ["react", "es2015", "stage-0"]
       },
       {
-        test: /\.(png|jpg)$/,
-        exclude: /(node_modules)/,
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+      },
+      {
+        test: /\.(png|jpg|gif)(\?v=\d+\.\d+\.\d+)?$/,
         loader: 'url-loader'
+      },
+      {
+        test: /\.(eot|com|json|ttf|woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/octet-stream'
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
       }
     ]
   }
